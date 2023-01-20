e7d9aa77fcef054d7b535fac82aeb76b76b965d2
Pingvu <nhlpool27@gmail.com>
2023-01-20 09:15:08 +0100

    Add project files.

---
+++ cs 2.1övning variabler/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Uppgift2
+{
+    class program
+    {
+        static void Main(string[]args)
+        {
+            Console.WriteLine("var är du ifrån?");
+            string plats = Console.ReadLine();
+            Console.WriteLine("vart vill du resa i framtiden");
+            string annanplats = Console.ReadLine();
+
+            Console.WriteLine("hej " + plats + "-bo. jag hoppas att du kan resa till " + annanplats);
+        }
+    }
+}
